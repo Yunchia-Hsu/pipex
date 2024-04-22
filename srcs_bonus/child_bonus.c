1c2dd38b305c73b29961a4adaa192bfb455999ed
Yun-Chia <127295359+Yunchia-Hsu@users.noreply.github.com>
2024-04-22 17:24:30 +0800

    Add files via upload

--- srcs_bonus/child_bonus.c
+++ srcs_bonus/child_bonus.c
@@ -12,6 +12,128 @@
 
 #include "pipex_bonus.h"
 
+
+static int	is_sep(char *str, int i, char *charset)
+{
+	int	j;
+	int	k;
+	int	flags;
+
+	j = -1;
+	while (charset[++j])
+	{
+		if (str[i] == charset[j])
+		{
+			k = -1;
+			flags = 0;
+			while (str[++k] && k <= i)
+			{
+				if (str[k] == '\\')
+					flags ^= (1 << 2);
+				if (str[k] == '"')
+					flags ^= (1 << 1);
+				if (str[k] == '\'')
+					flags ^= 1;
+			}
+			return (flags == 0);
+		}
+	}
+	return (str[i] == '\0');
+}
+
+static int	count_word(char *str, char *charset)
+{
+	int	word;
+	int	i;
+
+	word = 0;
+	i = 0;
+	while (str[i])
+	{
+		if (!is_sep(str, i, charset)
+			&& is_sep(str, i + 1, charset))
+			word++;
+		i++;
+	}
+	return (word);
+}
+
+static char	*write_word(char *dest, char *str, char *charset)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		while (is_sep(str, i, charset))
+			i++;
+		while (!is_sep(str, i, charset))
+		{
+			if (str[i] == '\\')
+				i++;
+			dest[j++] = str[i++];
+		}
+		break ;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+static char	**write_arr(char **arr, char *str, char *charset)
+{
+	int	len;
+	int	i;
+	int	word;
+
+	len = 0;
+	i = 0;
+	word = 0;
+	while (str[i])
+	{
+		if (is_sep(str, i, charset))
+			i++;
+		else
+		{
+			while (!is_sep(str, i + len, charset))
+				len++;
+			arr[word] = malloc(sizeof(char) * (len + 1));
+			if (arr[word] == NULL)
+				return (NULL);
+			arr[word] = write_word(arr[word], str + i, charset);
+			i += len;
+			len = 0;
+			word++;
+		}
+	}
+	return (arr);
+}
+
+/* 
+	A customized ft_split() function, which splits the string with valid 
+	separators. A separator is deemed as valid if it's outside any pair 
+	of quotes.
+*/
+
+char	**ft_split_pipex(char *str, char *charset)
+{
+	char	**arr;
+	int		word;
+
+	word = count_word(str, charset);
+	arr = (char **)malloc((word + 1) * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+	arr = write_arr(arr, str, charset);
+	arr[word] = NULL;
+	return (arr);
+}
+
+
+
+
+
 void close_fds(t_pipex *pipex)
 {
 	int i;
@@ -70,12 +192,63 @@ void	sub_dup2(int input, int output, t_pipex *pipex)
 		print_error("dup2 error", pipex, EXIT_FAILURE);
 }
 
+char *no_quote(char *cmd, t_pipex *pipex) //'{print}'
+{
+	char *result;
+	int new_len;
+	int i;
+
+	new_len = ft_strlen(cmd) - 1;
+	result = malloc(sizeof(char) * new_len);
+	if (result == NULL)
+		print_error("maloc error", pipex, EXIT_FAILURE);
+	i = 0;
+	while (i < new_len - 1)
+	{
+		result[i] = cmd[i + 1];
+		i++;	
+	}
+	result[i] = '\0';
+	free(cmd);
+	return(result);
+}  
+                                            // awk '{print}'
+char **get_cmd_arr(char *argv, t_pipex *pipex) //awk '{print}' -> cmd_arr0: awk / cmd_arr1: {print}
+{
+		char **cmd_arr;
+		int i;
+
+		dprintf(2, "argv: %s\n", argv);
+		cmd_arr = ft_split_pipex(argv, " ");
+		dprintf(2, "cmd_arr0: %s\n", cmd_arr[0] );
+		dprintf(2, "cmd_arr1: %s\n", cmd_arr[1] );
+		if (cmd_arr == NULL)
+			print_error("maloc error", pipex, EXIT_FAILURE);
+		
+		i = 0;
+		while(cmd_arr[i] != NULL)
+		{
+			if (ft_strlen(cmd_arr[i]) > 1)
+			{
+				if ((cmd_arr[i][0] == '\'' && cmd_arr[i][ft_strlen(cmd_arr[i]) - 1] == '\'') ||  //字符常量中包含一個單引號本身，就必須使用轉義序列 \'
+					(cmd_arr[i][0] == '"' && cmd_arr[i][ft_strlen(cmd_arr[i]) - 1 ] == '"')  )
+					cmd_arr[i] = no_quote(cmd_arr[i], pipex);
+			}
+			i++;
+		}
+		dprintf(2, "cmd_arr fianl 0: %s\n", cmd_arr[0] );
+		dprintf(2, "cmd_arr final 1: %s\n", cmd_arr[1] );
+	return (cmd_arr);	
+}
+
+
 void child(int i, char **argv, char **envp, t_pipex *pipex)
 {
 	if (i == 0)//1 cmd
 	{
 		sub_dup2(pipex->infile, pipex->pipe_fd[0][1], pipex);
-		pipex->cmd_arr = ft_split(argv[2 + pipex->here_doc], ' ');
+		//pipex->cmd_arr = ft_split(argv[2 + pipex->here_doc], ' ');
+		pipex->cmd_arr = get_cmd_arr(argv[2 + pipex->here_doc], pipex);
 	}
 	else if (i == (pipex->cmd_nbr - 1))//last cmd 3
 	{
