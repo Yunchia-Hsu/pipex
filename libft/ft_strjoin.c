55613633b777ea5ccb0efb554149f948b9749fa9
Yun-Chia <127295359+Yunchia-Hsu@users.noreply.github.com>
2024-04-26 00:43:11 +0300

    Add files via upload

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yhsu <yhsu@hive.student.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/01 13:07:47 by yhsu              #+#    #+#             */
+/*   Updated: 2024/04/18 13:56:03 by yhsu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+		char	*new_str;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+
+	len = ft_strlen(s1) + ft_strlen(s2) + 1;
+	new_str = (char *)malloc(sizeof(char) * len);
+	if (new_str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j])
+		new_str[i++] = s1[j++];
+	j = 0;
+	while (s2[j])
+		new_str[i++] = s2[j++];
+	new_str[i] = '\0';
+	return (new_str);
+}
